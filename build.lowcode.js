087c58a510c5c61863ede54f8b55c08337940784
liujuping <liujup@foxmail.com>
2023-03-02 15:18:50 +0800

    feat: update version of lowcode-engine and lowcode-materials

--- build.lowcode.js
+++ build.lowcode.js
@@ -17,16 +17,16 @@ module.exports = {
         library,
         staticResources: {
           engineCoreCssUrl:
-            'https://alifd.alicdn.com/npm/@alilc/lowcode-engine@1.1.3-beta.2/dist/css/engine-core.css',
+            'https://alifd.alicdn.com/npm/@alilc/lowcode-engine@1.1.3-beta.4/dist/css/engine-core.css',
           engineExtCssUrl:
             'https://alifd.alicdn.com/npm/@alilc/lowcode-engine-ext@1.0.5-beta.10/dist/css/engine-ext.css',
           engineCoreJsUrl:
-            'https://alifd.alicdn.com/npm/@alilc/lowcode-engine@1.1.3-beta.2/dist/js/engine-core.js',
+            'https://alifd.alicdn.com/npm/@alilc/lowcode-engine@1.1.3-beta.4/dist/js/engine-core.js',
           engineExtJsUrl:
             'https://alifd.alicdn.com/npm/@alilc/lowcode-engine-ext@1.0.5-beta.10/dist/js/engine-ext.js',
         },
         extraAssets: [
-          'https://g.alicdn.com/code/npm/@ali/ali-lowcode-materials/1.2.4/assets.json',
+          'https://alifd.alicdn.com/npm/@alilc/lowcode-materials@1.0.6/dist/assets.json',
           'https://g.alicdn.com/code/npm/@alife/fusion-ui/0.1.7/build/lowcode/assets-prod.json',
         ],
         renderUrls: [`${baseRenderUrl}/dist/${library}.js`, `${baseRenderUrl}/dist/${library}.css`],
